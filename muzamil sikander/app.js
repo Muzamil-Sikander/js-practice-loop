f42f95f11248cc5b9b2159c674fab84df07adab4
muzamilsikander <102538533+Muzamil-Sikander@users.noreply.github.com>
2022-06-11 21:51:19 +0500

    jspractice

---
+++ muzamil sikander/app.js
@@ -0,0 +1,44 @@
+// loop
+// question#1
+
+// for( var i=0; i=<5; i++){
+//  document.write("hello world" + "<br>" )
+// }
+
+// question#2
+
+// for(var i=1; i<=10; i++){
+//     document.write( i + "<br>");
+// }
+// question#3
+// var table = prompt("Enter Your Table Number");
+// var length = prompt("Enter your  Length ");
+// for( var i = 1; i <= length; i++){
+//      document.write( table + "X" + i + "=" + table*i + "<br>");
+// }
+//  question#4
+  
+//  var array = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"];
+//  for(var i=0 ; i < array.length; i++){
+//      document.write(array[i] + "<br>")
+//  }
+
+// question#5
+
+// var array = ["apple", "banana", "mango", "orange", "strawberry"];
+// for(var i=0 ; i < array.length; i++){
+//     document.write( array[i] + "<br>")
+// }
+
+
+//  var array = ["apple", "banana", "mango", "orange", "strawberry"];
+//    for(var i=0 ; i < array.length; i++){
+//      document.write(  "<br>" + " element at index  " + i + " is " +array[i] + "<br>"  )
+
+
+//     }
+
+// question#6
+
+
+    
